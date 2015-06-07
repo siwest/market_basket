032353be2018cfdbb5d0aec8499cc4cbcb1fe58d
Sarah West <sarah.irene.west@gmail.com>
2015-06-06 21:36:18 -0400

    updated confidence def

--- market_basket.py
+++ market_basket.py
@@ -3,10 +3,6 @@
 
 import csv
 
-
-
-print("Hello world")
-
 def read_CSV(file_name):
 
     result = list()
@@ -35,10 +31,19 @@ def support_frequency(orders, item_set):
     N = len(orders[1:])
     return support_count(orders, item_set)/N
 
+def confidence(orders, left, right):
+    left_count = support_count(orders, left)
+    right = right.union(left)
+    right_count = support_count(orders, right)
+    result = right_count/left_count
+    return result
+
 def main():
     data = read_CSV('market_basket.csv')
-    item_set = set(['Eggs','Bread','Bananas'])
+    item_set = set(['Eggs','Bread'])
+    item_set2 = set(['Spinach'])
     print(support_frequency(data, item_set))
+    print(confidence(data, item_set, item_set2))
 
 
 if __name__ == '__main__':
