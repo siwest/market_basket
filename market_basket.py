2f2a5594c1555b7333a724f580125dfcefb46725
Kent Horvath <kenthorvath@gmail.com>
2015-06-14 16:19:14 -0400

    Formats to PEP8 standard.

--- market_basket.py
+++ market_basket.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-# Market basket python program
+"""Market basket python program."""
 
 import csv
 import itertools
 
-def read_CSV(file_name):
 
+def read_CSV(file_name):
+    """TODO: Insert docstring here."""
     result = list()
     with open(file_name, 'r') as csv_file:
         file_reader = csv.reader(csv_file)
@@ -17,31 +18,39 @@ def read_CSV(file_name):
 
 
 def support_count(orders, item_set):
+    """TODO: Insert docstring here."""
     count = 1
 
     for order in orders[1:]:
         if item_set.issubset(order):
-            #print("Found {} in {}".format(item_set, order))
+            # print("Found {} in {}".format(item_set, order))
             count += 1
         else:
-            #print("Didn't find {} in {}".format(item_set, order))
+            # print("Didn't find {} in {}".format(item_set, order))
             pass
     return count
 
+
 def support_frequency(orders, item_set):
+    """TODO: Insert docstring here."""
     N = len(orders)
     return support_count(orders, item_set)/float(N)
 
+
 def confidence(orders, left, right):
+    """TODO: Insert docstring here."""
     left_count = support_count(orders, left)
     right = right.union(left)
     right_count = support_count(orders, right)
     result = right_count/left_count
     return result
 
+
 def generate_candidate_set(orders, inventory_list, support_threshold):
+    """TODO: Insert docstring here."""
     candidate_set = set()
-    while (inventory_list): # find frequent item sets, save in candidate_set
+    while (inventory_list):
+        # find frequent item sets, save in candidate_set
         unique_item = inventory_list.pop()
 
         candidate_set.add(unique_item)
@@ -52,38 +61,41 @@ def generate_candidate_set(orders, inventory_list, support_threshold):
         s = support_frequency(orders, temp_set)
 
         if (s < support_threshold):
-            print(str(unique_item) + ' ' + str(s) + " does not meet threshold ")
+            print(
+                str(unique_item) + ' ' + str(s) + " does not meet threshold ")
             candidate_set.remove(unique_item)
         else:
             print(str(unique_item) + ' ' + str(s) + " meets threshold ")
     pass
 
-
-    #print ('\nCandidate Set: \n' + str(candidate_set))
+    # print ('\nCandidate Set: \n' + str(candidate_set))
     return candidate_set
 
+
 def apriori(orders, support_threshold):
-    #frequency_list1 = list()
+    """TODO: Insert docstring here."""
+    # frequency_list1 = list()
     N = len(orders[1:])
 
-    inventory_list = orders[0]  #inventory_list is the header of the csv file
+    inventory_list = orders[0]  # inventory_list is the header of the csv file
     print('Inventory List: \n' + str(inventory_list) + '\n')
 
     test_set = orders
     transaction = test_set.pop()
 
-    one_item_candidate_set = set() #find all frequen 1-itemsets
-    one_item_candidate_set = generate_candidate_set(orders, inventory_list, support_threshold)
+    one_item_candidate_set = set()  # find all frequen 1-itemsets
+    one_item_candidate_set = generate_candidate_set(
+        orders, inventory_list, support_threshold)
 
     k_item_candidate_set = one_item_candidate_set
 
     k = 2
-    while(k<N):
+    while(k < N):
         k_item_set = set(itertools.combinations(k_item_candidate_set, k))
-        k_item_candidate_set = generate_candidate_set(orders, k_item_set, support_threshold)
-        k=k+1
+        k_item_candidate_set = generate_candidate_set(
+            orders, k_item_set, support_threshold)
+        k = k+1
     pass
-
 
     # k = len(one_item_candidate_set)
     # for candidate in one_item_candidate_set: #extract the frequent k-itemsets
@@ -92,16 +104,13 @@ def apriori(orders, support_threshold):
     #     for
 
 
-
-
 def main():
+    """TODO: Insert docstring here."""
     data = read_CSV('market_basket.csv')
-   # item_set = set(['Eggs','Bread'])
-   # item_set2 = set(['Spinach'])
-   # print(support_frequency(data, item_set))
-   # print(confidence(data, item_set, item_set2))
-
-
+    # item_set = set(['Eggs','Bread'])
+    # item_set2 = set(['Spinach'])
+    # print(support_frequency(data, item_set))
+    # print(confidence(data, item_set, item_set2))
 
     apriori(data, .3)
 
