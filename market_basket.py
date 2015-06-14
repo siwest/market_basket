7afaab43d4e09060ca93bf3af71cd53f8ad893ab
Kent Horvath <kenthorvath@gmail.com>
2015-06-14 19:36:11 -0400

    Preliminary working version.

--- market_basket.py
+++ market_basket.py
@@ -72,36 +72,62 @@ def generate_candidate_set(orders, inventory_list, support_threshold):
     return candidate_set
 
 
-def apriori(orders, support_threshold):
-    """TODO: Insert docstring here."""
-    # frequency_list1 = list()
-    N = len(orders[1:])
+def apriori(orders, support_threshold, confidence_threshold):
+    """Accepts a list of item sets (i.e. orders) and returns a list of
+    association rules matching support and confidence thresholds. """
+    candidate_items = set()
+    for items in orders:
+        candidate_items = candidate_items.union(items)
 
-    inventory_list = orders[0]  # inventory_list is the header of the csv file
-    print('Inventory List: \n' + str(inventory_list) + '\n')
+    print("Candidate items are {}".format(candidate_items))
 
-    test_set = orders
-    transaction = test_set.pop()
+    def apriori_next(item_set=set()):
+        """Accepts a single item set and returns list of all association rules
+        containing item_set that match support and confidence thresholds.
+        """
+        result = []
 
-    one_item_candidate_set = set()  # find all frequen 1-itemsets
-    one_item_candidate_set = generate_candidate_set(
-        orders, inventory_list, support_threshold)
+        # print("Calling APN with {}".format(item_set))
+        # print("Candidates are {}".format(candidate_items))
 
-    k_item_candidate_set = one_item_candidate_set
+        if len(item_set) == len(candidate_items):
+            # Recursion base case.
+            print("{} == {}".format(item_set, candidate_items))
+            return result
 
-    k = 2
-    while(k < N):
-        k_item_set = set(itertools.combinations(k_item_candidate_set, k))
-        k_item_candidate_set = generate_candidate_set(
-            orders, k_item_set, support_threshold)
-        k = k+1
-    pass
+        elif not item_set:
+            # Initialize with every item meeting support threshold.
+            print("Initializing APN.\n")
+            for item in candidate_items:
+                item_set = {item}
+                if support_frequency(orders, item_set) > support_threshold:
+                    #print("Item '{}' crosses support threshold".format(item))
+                    result.extend(apriori_next(item_set))
+                else:
+                    pass
 
-    # k = len(one_item_candidate_set)
-    # for candidate in one_item_candidate_set: #extract the frequent k-itemsets
-    #     new_item_set = set()
-    #     new_item_set.add(candidate)
-    #     for
+        else:
+            # Given an item set, find all candidate items meeting thresholds
+            for item in candidate_items.difference(item_set):
+                #print("Testing {}".format(item_set.union({item})))
+                if support_frequency(orders, item_set.union({item})) >\
+                        support_threshold:
+                    print("\n\nItem set {} crosses support threshold".format(
+                        item_set.union({item})))
+                    if confidence(orders, item_set, {item}) >\
+                            confidence_threshold:
+                        print("\n\n{} => {} crosses confidence threshold".format(
+                            item_set, item))
+                        result.append((item_set, item))
+                        result.extend(apriori_next(item_set.union({item})))
+                    else:
+                        pass
+                else:
+                    pass
+
+        return [rule for rule in result if rule]
+
+    return apriori_next()
 
 
 def main():
@@ -112,7 +138,8 @@ def main():
     # print(support_frequency(data, item_set))
     # print(confidence(data, item_set, item_set2))
 
-    apriori(data, .3)
+    print(["{} => {}".format(item_set, item) for item_set, item in apriori(
+        data, 0.1, 0.1)])
 
 
 if __name__ == '__main__':
