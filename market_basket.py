b3cc3eaefcbd2c85daf0d08e4622e3f4f538b6a1
Sarah West <sarah.irene.west@gmail.com>
2015-06-20 11:46:07 -0400

    Adds extra csv files, corrects apriori to work with mixed formats

--- market_basket.py
+++ market_basket.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 """Market basket python program."""
 
-import csv
-import itertools
+import sys, getopt
 
-
-def read_CSV(file_name):
-    """TODO: Insert docstring here."""
+def read_data(file_name):
+    """Read a csv file that lists possible transactions"""
     result = list()
-    with open(file_name, 'r') as csv_file:
-        file_reader = csv.reader(csv_file)
-        for row in file_reader:
-            order_set = set(row)-set(['0'])
+    with open(file_name, 'r') as file_reader:
+        for line in file_reader:
+            order_set = set(line.strip().split(','))
             result.append(order_set)
-    # print(result)
     return result
 
 
 def support_count(orders, item_set):
-    """TODO: Insert docstring here."""
-    count = 1
+    """Calculate support count of item set from orders 2D list"""
+    count = 0
 
-    for order in orders[1:]:
+    for order in orders:
         if item_set.issubset(order):
             # print("Found {} in {}".format(item_set, order))
             count += 1
@@ -32,13 +28,13 @@ def support_count(orders, item_set):
 
 
 def support_frequency(orders, item_set):
-    """TODO: Insert docstring here."""
+    """Calculate support frequency of item set from orders 2D list"""
     N = len(orders)
     return support_count(orders, item_set)/float(N)
 
 
 def confidence(orders, left, right):
-    """TODO: Insert docstring here."""
+    """Calculate confidence of item set from orders 2D list"""
     left_count = support_count(orders, left)
     right = right.union(left)
     right_count = support_count(orders, right)
@@ -46,40 +42,15 @@ def confidence(orders, left, right):
     return result
 
 
-def generate_candidate_set(orders, inventory_list, support_threshold):
-    """TODO: Insert docstring here."""
-    candidate_set = set()
-    while (inventory_list):
-        # find frequent item sets, save in candidate_set
-        unique_item = inventory_list.pop()
-
-        candidate_set.add(unique_item)
-
-        temp_set = set()
-        temp_set.add(unique_item)
-
-        s = support_frequency(orders, temp_set)
-
-        if (s < support_threshold):
-            print(
-                str(unique_item) + ' ' + str(s) + " does not meet threshold ")
-            candidate_set.remove(unique_item)
-        else:
-            print(str(unique_item) + ' ' + str(s) + " meets threshold ")
-    pass
-
-    # print ('\nCandidate Set: \n' + str(candidate_set))
-    return candidate_set
-
-
 def apriori(orders, support_threshold, confidence_threshold):
     """Accepts a list of item sets (i.e. orders) and returns a list of
     association rules matching support and confidence thresholds. """
     candidate_items = set()
+
     for items in orders:
         candidate_items = candidate_items.union(items)
 
-    print("Candidate items are {}".format(candidate_items))
+    # print("Candidate items are {}".format(candidate_items))
 
     def apriori_next(item_set=set()):
         """Accepts a single item set and returns list of all association rules
@@ -92,16 +63,16 @@ def apriori(orders, support_threshold, confidence_threshold):
 
         if len(item_set) == len(candidate_items):
             # Recursion base case.
-            print("{} == {}".format(item_set, candidate_items))
+            # print("{} == {}".format(item_set, candidate_items))
             return result
 
         elif not item_set:
             # Initialize with every item meeting support threshold.
-            print("Initializing APN.\n")
+            # print("Initializing APN.\n")
             for item in candidate_items:
                 item_set = {item}
-                if support_frequency(orders, item_set) > support_threshold:
-                    #print("Item '{}' crosses support threshold".format(item))
+                if support_frequency(orders, item_set) >= support_threshold:
+                    # print("Item '{}' crosses support threshold".format(item))
                     result.extend(apriori_next(item_set))
                 else:
                     pass
@@ -109,15 +80,13 @@ def apriori(orders, support_threshold, confidence_threshold):
         else:
             # Given an item set, find all candidate items meeting thresholds
             for item in candidate_items.difference(item_set):
-                #print("Testing {}".format(item_set.union({item})))
-                if support_frequency(orders, item_set.union({item})) >\
-                        support_threshold:
-                    print("\n\nItem set {} crosses support threshold".format(
-                        item_set.union({item})))
-                    if confidence(orders, item_set, {item}) >\
-                            confidence_threshold:
-                        print("\n\n{} => {} crosses confidence threshold".format(
-                            item_set, item))
+                # print("Testing {}".format(item_set.union({item})))
+                if confidence(orders, item_set, {item}) >=\
+                        confidence_threshold:
+                    # print("\n\n{} => {} crosses confidence threshold at {}".format(item_set, item, confidence(orders, item_set, {item})))
+                    if support_frequency(orders, item_set.union({item})) >=\
+                            support_threshold:
+                       #print("\nItem set {} crosses support threshold at {}".format(item_set.union({item}), support_frequency(orders, item_set.union({item}))))
                         result.append((item_set, item))
                         result.extend(apriori_next(item_set.union({item})))
                     else:
@@ -130,17 +99,33 @@ def apriori(orders, support_threshold, confidence_threshold):
     return apriori_next()
 
 
-def main():
-    """TODO: Insert docstring here."""
-    data = read_CSV('market_basket.csv')
-    # item_set = set(['Eggs','Bread'])
-    # item_set2 = set(['Spinach'])
-    # print(support_frequency(data, item_set))
-    # print(confidence(data, item_set, item_set2))
+def main(argv):
+    """Add command line arguments file name, support threshold, and confidence threshold"""
+    try:
+      opts, args = getopt.getopt(argv,"hf:s:c:",["support_threshold=","confidence_threshold="])
+    except getopt.GetoptError:
+      print ('file.csv -s <support_threshold> -c <confidence_threshold>')
+      sys.exit(2)
 
-    print(["{} => {}".format(item_set, item) for item_set, item in apriori(
-        data, 0.1, 0.1)])
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('file.csv -s <support> -c <confidence>')
+            sys.exit()
+        elif opt in ("-f", "--file"):
+            infile = arg
+        elif opt in ("-s", "--support"):
+            support_threshold = float(arg)
+        elif opt in ("-c", "--confidence"):
+            confidence_threshold = float(arg)
 
+    data = read_data(infile)
+
+    final_results = ["{} => {}  s = {:0.2f}, c = {:0.2f}".format(
+        item_set, item, support_frequency(data[1:], item_set.union({item})),
+        confidence(data[1:], item_set, {item})) for item_set, item in apriori(
+        data[1:], support_threshold, confidence_threshold)]
+    for result in final_results:
+        print(result)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
